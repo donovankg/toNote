13420b8572c7b6af9d95e1170f859713c9716b99
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-09-22 12:46:01 -0500

    added require.js and made the createnote function into a module

---
+++ js/createNote.js
@@ -0,0 +1,25 @@
+define(function () {
+	var article = document.getElementsByTagName('article')[0];   
+	function addNewNote(event){
+		var make = new Note();
+
+	}
+    function  Note (title,description){
+		var newDiv = document.createElement('div');
+		var newX = document.createElement('button');
+		var newTxt = document.createElement('p');
+		var newTitle = document.createElement('p');
+		article.appendChild(newDiv);
+		newDiv.appendChild(newTitle);
+		newDiv.appendChild(newX);
+		newDiv.appendChild(newTxt);
+		newDiv.className="addNote";
+		newX.className="xBtn";
+		newTitle.className = "noteTitle";
+		newTxt.className = "noteContent";
+		newX.innerHTML = "X";
+		newTitle.innerHTML = "title place holder";
+		newTxt.innerHTML= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	}
+	return addNewNote;
+});
