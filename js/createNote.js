b20413759eb0d13675b9dc2f5c6f8d2f03faf7e2
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-09-22 16:27:20 -0500

    fixed issue with editing text. need to fix css locations of new items.

--- js/createNote.js
+++ js/createNote.js
@@ -19,8 +19,8 @@ define(function () {
 		newTitle.className = "noteTitle";
 		newTxt.className = "noteContent";
 		newX.innerHTML = "X";
-		newTitle.innerHTML = "title place holder";
-		newTxt.innerHTML= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		newTitle.innerHTML = "";
+		newTxt.innerHTML= "";
 	}
 	return addNewNote;
 });
--- js/selected.js
+++ js/selected.js
@@ -2,31 +2,45 @@ define(function () {
 	var article = document.getElementsByTagName('article')[0];   
 	var editBox = document.createElement('div');
 	var titleInput = document.createElement('input');
+	var contentInput = document.createElement('textarea');
 	article.addEventListener('click',selectThis, false);
-
+	article.addEventListener('click',saveNote, false);
+	var currentNote;
 	var title ="";
 	var content="";
+
 	function selectThis(event){
 		if(event.target.className=="addNote new" || event.target.className=="addNote"){
-			
-			title = event.target.childNodes[0].innerHTML;
-			content = event.target.childNodes[2].innerHTML;
-			event.target.className="addNote selected";
+			currentNote = event.target;
+			title = currentNote.childNodes[0].innerHTML;
+			content = currentNote.childNodes[2].innerHTML;
+			currentNote.className="addNote selected";
 			article.appendChild(editBox);
 			editBox.className="edit";
 			editBox.appendChild(titleInput);
 			titleInput.value = title;
-
+			titleInput.className="titleInput"
+			editBox.appendChild(contentInput);
+			contentInput.value = content;
+			contentInput.className = "contentInput";
+			var title = event.target.childNodes[0].innerHTML;
+			
 	}
+}
 //		}else if(event.target.className=="addNote selected"){
 			//if(!event.target.className=="noteTitle"||!event.target.className=="noteContent"){
 //				event.target.className="addNote";
-	if(event.target.className =="saveNote"){
+	function saveNote(event){
+		if(event.target.className=="edit"){
+			console.log(title);
+			// newTitle.innerHTML = title;
+			// newTxt.innerHTML= content;
+			currentNote.childNodes[0].innerHTML = titleInput.value;
+			currentNote.childNodes[2].innerHTML = contentInput.value;
+			event.target.remove();
+
 
 	}
-				
-
-				
 		}
-	}
+	
 });
