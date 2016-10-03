90f32e1e78f6cb69a65872248b13318589ac35e1
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-10-02 21:59:04 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-requirejs(['js/createNote','js/selected'],function(fromCreateNote, fromSlected){
+requirejs(['js/createNote','js/selected','js/add'],function(fromCreateNote, fromSlected,fromAdd){
 	//localStorage.setItem(0,0);
 	var article = document.getElementsByTagName('article')[0];
 	var addBtn = document.getElementById('addBtn');
 	addBtn.addEventListener('click', fromCreateNote, false);
-
+	article.addEventListener('load', fromAdd,true);
 //remove note
+
 	var removeNote = function (event){
 		var newDiv = document.createElement('div');
 		if(event.target.className=="xBtn"){
@@ -21,19 +22,19 @@ requirejs(['js/createNote','js/selected'],function(fromCreateNote, fromSlected){
 };
 article.addEventListener('click',removeNote, false);
 })
-//var node = JSON.parse(localStorage[key]);
-var article = document.getElementsByTagName('article')[0];  //adds to the local after putting this in a mudule.
-var getDate;
-var newDiv = document.createElement('div');
-var newX = document.createElement('button');
-var newTxt = document.createElement('p');
-var newTitle = document.createElement('p');
-var newDate = document.createElement('p');
-var editDate = document.createElement('p');
+
 if(localStorage.length > 0){
 	(function(){
 		for(var key in localStorage){
 			var node = JSON.parse(localStorage[key]);
+			var article = document.getElementsByTagName('article')[0];  //adds to the local after putting this in a mudule.
+			var getDate;
+			var newDiv = document.createElement('div');
+			var newX = document.createElement('button');
+			var newTxt = document.createElement('p');
+			var newTitle = document.createElement('p');
+			var newDate = document.createElement('p');
+			var editDate = document.createElement('p');
 			newDiv.IdName = key;
 			article.appendChild(newDiv);
 			newDiv.appendChild(newTitle);
